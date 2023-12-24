f9666a16aa9c81a013cbc1589e918285f1289e6d
Wessel Bindt <wesselbindt+gitlab@gmail.com>
2023-12-25 00:23:53 +0100

    Tighten up types

--- property_based_testing/api.py
+++ property_based_testing/api.py
@@ -1,11 +1,14 @@
-from typing import TypeVar, Callable
+from typing import TypeVar, Callable, TypeVarTuple
 
 T = TypeVar('T')
+Ts = TypeVarTuple('Ts')
 
 
 def inject(
-    dependency_generator: Callable[[], T ],
-) -> Callable[[Callable[..., None]], Callable[..., None]]:
-    def decorator(test: Callable[..., None]) -> Callable[..., None]:
-        return lambda *args, **kwargs: test(dependency_generator(), *args, **kwargs)
+    dependency_generator: Callable[[], T],
+) -> Callable[[Callable[[T, *Ts], None]], Callable[[*Ts], None]]:
+    def decorator(test: Callable[[T, *Ts], None]) -> Callable[[*Ts], None]:
+        def curried_test(*remaining_dependencies: *Ts) -> None:
+            test(dependency_generator(), *remaining_dependencies)
+        return curried_test
     return decorator
