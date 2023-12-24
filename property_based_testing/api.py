3058b44c48e02b7255bf2e95a134ae43e15213e4
Wessel Bindt <wesselbindt+gitlab@gmail.com>
2023-12-25 00:21:10 +0100

    Inject arguments

--- property_based_testing/api.py
+++ property_based_testing/api.py
@@ -1,2 +1,11 @@
-def inject():
-    pass
+from typing import TypeVar, Callable
+
+T = TypeVar('T')
+
+
+def inject(
+    dependency_generator: Callable[[], T ],
+) -> Callable[[Callable[..., None]], Callable[..., None]]:
+    def decorator(test: Callable[..., None]) -> Callable[..., None]:
+        return lambda *args, **kwargs: test(dependency_generator(), *args, **kwargs)
+    return decorator
--- test_property_based_testing/test_inject.py
+++ test_property_based_testing/test_inject.py
@@ -1,3 +1,4 @@
+from property_based_testing.api import inject
 from property_based_testing.internal import run_property_test, Success, Failure
 from test_property_based_testing.code_to_test_with import square
 
@@ -14,3 +15,14 @@ def test_run_property_based_test_runs_unsuccessful_tests_without_arguments() -> 
         assert square(1) == 19
 
     assert run_property_test(property_test_that_1_squared_is_19) == Failure()
+
+
+def test_inject_actually_injects_arguments() -> None:
+    def positive_integer() -> int:
+        return 3
+
+    @inject(positive_integer)
+    def property_test_squares_are_nonnegative(a: int) -> None:
+        assert square(a) >= 0
+
+    assert run_property_test(property_test_squares_are_nonnegative) == Success()
