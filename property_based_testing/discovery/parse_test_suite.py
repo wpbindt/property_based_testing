a2eedc5eb0d63198a2702501de799eb3035396c4
Wessel Bindt <wesselbindt+gitlab@gmail.com>
2023-12-26 14:44:45 +0100

    Actually get module members when parsing test suite

--- property_based_testing/discovery/parse_test_suite.py
+++ property_based_testing/discovery/parse_test_suite.py
@@ -1,7 +1,11 @@
+from inspect import getmembers, isfunction
 from types import ModuleType
 
 from property_based_testing.internal import FullyInjectedPropertyTest
 
 
 def parse_test_suite(module: ModuleType) -> list[FullyInjectedPropertyTest]:
-    return []
+    return [
+        lambda: None
+        for _ in getmembers(module, predicate=isfunction)
+    ]
--- test_property_based_testing/test_discovery/test_parse_module.py
+++ test_property_based_testing/test_discovery/test_parse_module.py
@@ -1,6 +1,10 @@
 from property_based_testing.discovery.parse_test_suite import parse_test_suite
-from test_property_based_testing.test_discovery import empty_test_suite
+from test_property_based_testing.test_discovery import empty_test_suite, property_test_suite_with_one_test
 
 
 def test_parsing_suite_without_tests_succeeds() -> None:
     assert parse_test_suite(empty_test_suite) == []
+
+
+def test_parsing_suite_with_one_test_finds_one_test() -> None:
+    assert len(parse_test_suite(property_test_suite_with_one_test)) == 1
