8696733d7d66935f533b25b1347f9ccd7c171e0a
Wessel Bindt <wesselbindt+gitlab@gmail.com>
2023-12-27 20:57:30 +0100

    enable testing

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env sh
 python setup.py sdist
-twine upload --non-interactive dist/*
+#twine upload --non-interactive dist/*
+echo deploying version $(cat property_based_testing/__init__.py)
